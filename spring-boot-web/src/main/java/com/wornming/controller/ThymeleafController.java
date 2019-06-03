844f476ef6880b4048cfd0ab6c1e8182eb89b909
wornming <liwp20979@hundsun.com>
2019-06-04 00:04:08 +0800

    增加webjar统一管理静态资源。

---
+++ spring-boot-web/src/main/java/com/wornming/controller/ThymeleafController.java
@@ -0,0 +1,30 @@
+package com.wornming.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * ThymeleafController示例
+ * @author wornming
+ * @version 1.0
+ * @since 2019/6/3 23:16
+ */
+@Controller
+public class ThymeleafController {
+    @RequestMapping("/hi")
+    public String hello(Locale locale, Model model) {
+        model.addAttribute("greeting", "Hello!");
+
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+        String formattedDate = dateFormat.format(date);
+        model.addAttribute("currentTime", formattedDate);
+
+        return "hello";
+    }
+}
